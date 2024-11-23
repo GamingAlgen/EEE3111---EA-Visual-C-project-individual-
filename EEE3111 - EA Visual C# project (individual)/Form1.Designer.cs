6e7799c10e3aaee3a29700edd1fbabd6978fc1cd
GamingAlgen <69628480+GamingAlgen@users.noreply.github.com>
2024-11-23 20:41:11 +0800

    loginscreen basics done

--- EEE3111 - EA Visual C# project (individual)/Form1.Designer.cs
+++ EEE3111 - EA Visual C# project (individual)/Form1.Designer.cs
@@ -28,34 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.textboxPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textboxUser = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.linkLabelForgotPassword = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // button1
+            // loginButton
             // 
-            this.button1.Location = new System.Drawing.Point(299, 270);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(177, 59);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.Location = new System.Drawing.Point(346, 297);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(127, 29);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Login";
+            this.loginButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // textboxPassword
+            // 
+            this.textboxPassword.BackColor = System.Drawing.SystemColors.Window;
+            this.textboxPassword.Font = new System.Drawing.Font("PMingLiU", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textboxPassword.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textboxPassword.Location = new System.Drawing.Point(346, 197);
+            this.textboxPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textboxPassword.Name = "textboxPassword";
+            this.textboxPassword.Size = new System.Drawing.Size(127, 29);
+            this.textboxPassword.TabIndex = 8;
+            this.textboxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelPassword.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelPassword.Location = new System.Drawing.Point(243, 197);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(86, 19);
+            this.labelPassword.TabIndex = 7;
+            this.labelPassword.Text = "Password";
+            // 
+            // textboxUser
+            // 
+            this.textboxUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textboxUser.Font = new System.Drawing.Font("PMingLiU", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textboxUser.Location = new System.Drawing.Point(346, 98);
+            this.textboxUser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textboxUser.Name = "textboxUser";
+            this.textboxUser.Size = new System.Drawing.Size(127, 29);
+            this.textboxUser.TabIndex = 6;
+            this.textboxUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelUsername.Location = new System.Drawing.Point(218, 100);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(100, 19);
+            this.labelUsername.TabIndex = 5;
+            this.labelUsername.Text = "User Name";
+            // 
+            // linkLabelForgotPassword
+            // 
+            this.linkLabelForgotPassword.AutoSize = true;
+            this.linkLabelForgotPassword.Location = new System.Drawing.Point(343, 329);
+            this.linkLabelForgotPassword.Name = "linkLabelForgotPassword";
+            this.linkLabelForgotPassword.Size = new System.Drawing.Size(86, 13);
+            this.linkLabelForgotPassword.TabIndex = 9;
+            this.linkLabelForgotPassword.TabStop = true;
+            this.linkLabelForgotPassword.Text = "Forgot Password";
+            this.linkLabelForgotPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelForgotPassword_LinkClicked);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabelForgotPassword);
+            this.Controls.Add(this.textboxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textboxUser);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.loginButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox textboxPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textboxUser;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.LinkLabel linkLabelForgotPassword;
     }
 }
 
--- EEE3111 - EA Visual C# project (individual)/Form1.cs
+++ EEE3111 - EA Visual C# project (individual)/Form1.cs
@@ -12,6 +12,9 @@ namespace EEE3111___EA_Visual_C__project__individual_
 {
     public partial class Form1 : Form
     {
+        private string password = "admin";
+        private string username = "admin";
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +23,28 @@ namespace EEE3111___EA_Visual_C__project__individual_
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (textboxUser.Text != username || textboxPassword.Text != password)
+            {
+                MessageBox.Show("Error, invalid username or password.", "Login Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Login Successful!", "Welcome", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        private void linkLabelForgotPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Please contact support to reset your password.", "Password Reset", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
